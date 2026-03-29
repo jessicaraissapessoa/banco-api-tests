590079199c729706ee0de6445058143119ee8bec
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-03-29 00:52:48 -0300

    Adicionei testes para GET transferências total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,9 +5,10 @@ const { obterToken } = require('../helpers/autenticacao')
 const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
+
+    let token
+
     describe('POST /transferencias', () => {
-        
-        let token
         
         beforeEach(async () => {
             token = await obterToken('julio.lima', '123456')
@@ -39,6 +40,42 @@ describe('Transferências', () => {
                 .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(422)
+            console.log(resposta.body)
+        })
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e os dados da transferência', async () => {
+
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/15')
+                .set('Authorization', `Bearer ${token}`)
+            
+                expect(resposta.status).to.equal(200)
+                expect(resposta.body.id).to.equal(15)
+                expect(resposta.body.id).to.be.a('number')
+                expect(resposta.body.conta_origem_id).to.equal(1)
+                expect(resposta.body.conta_destino_id).to.equal(2)
+                //expect(resposta.body.valor).to.equal(11)
+                expect(resposta.body.data_hora).to.be.a('string')
+                expect(resposta.body.autenticada).to.equal(0)
+                
+                console.log(resposta.status)
+                console.log(resposta.body)
+        })
+    })
+
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+            
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.lengthOf(10)
+
             console.log(resposta.body)
         })
     })
